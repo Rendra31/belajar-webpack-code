e3d1b9a2c2e8a3314f117ee5d88ea9c5408b3c1a
travelite <rendrabootcamp@gmail.com>
2022-11-18 21:29:37 +0700

    sample implement minimizer

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,15 @@
-const path = require("path");
 const { merge } = require("webpack-merge");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const glob = require("glob");
 const config = require("./webpack.config");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+const PATHS = {
+  src: path.join(__dirname, "src"),
+};
 
 module.exports = merge(config, {
   mode: "production",
@@ -11,9 +19,26 @@ module.exports = merge(config, {
     assetModuleFilename: "img/[name].[hash][ext]",
     clean: true,
   },
+  optimization: {
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin(),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.squooshMinify,
+          options: {
+            // Your options for `squoosh`
+          },
+        },
+      }),
+    ],
+  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: "main.[contenthash].css",
+    }),
+    new PurgeCSSPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
     }),
   ],
 });
